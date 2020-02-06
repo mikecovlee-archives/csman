90e2c2db36236a9cbda99659ab2e593c9d796d16
imkiva <libkernelpanic@gmail.com>
2020-02-06 17:52:52 +0800

    cli: progress bar

---
+++ csman-cli/src/progress.cpp
@@ -0,0 +1,78 @@
+//
+// Created by kiva on 2020/1/31.
+//
+
+#include <chrono>
+#include <csman/os/os.hpp>
+#include <csman/cli/progress.hpp>
+
+namespace csman {
+    namespace cli {
+        using namespace csman::os;
+
+        void progress_bar::start() {
+            _thread = std::make_unique<std::thread>(
+                [](progress_bar &bar) {
+                    static constexpr char C[] = {
+                        '/', '-', '\\', '|',
+                    };
+
+                    int width = bar._width;
+                    size_t index = 0;
+                    auto *os = OS::current();
+
+                    while (bar._draw) {
+                        int progress = bar._progress;
+                        if (index >= sizeof(C)) {
+                            index = 0;
+                        }
+
+                        printf("  %c  %3d%% ", C[index++ % sizeof(C)], progress);
+
+                        int complete = width * progress / 100;
+                        int empty = width - complete;
+
+                        // progress bar start mark
+                        putchar('[');
+
+                        // finished part
+                        for (int i = 0; i < complete - 1; ++i) {
+                            putchar('=');
+                        }
+
+                        // processing part
+                        putchar('>');
+
+                        // future part
+                        for (int i = 0; i < empty; ++i) {
+                            putchar(' ');
+                        }
+
+                        // progress bar end mark
+                        putchar(']');
+
+                        // workaround for windows terminal:
+                        // fix duplicated tailing progress bar end mark
+                        putchar(' ');
+
+                        // display it!
+                        os->rewind_cursor();
+                        fflush(stdout);
+                        std::this_thread::sleep_for(std::chrono::milliseconds (100));
+                    }
+
+                    os->rewind_cursor();
+                    printf("  ✔︎\n");
+                },
+                std::ref(*this));
+        }
+
+        progress_bar::~progress_bar() {
+            stop();
+            if (_thread != nullptr && _thread->joinable()) {
+                _thread->join();
+            }
+        }
+    }
+}
+
